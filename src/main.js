aea3c776d3c10693e7463ca990af58381fea574a
OSandh <oskarsandh@gmail.com>
2019-11-13 14:06:51 +0100

    Update package.json, main.js, and routes.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Routes from './routes.js'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+
+Vue.use(VueRouter);
+Vue.use(VueAxios, axios)
+
+const xjRouter = new VueRouter({
+  routes: Routes,
+  mode: 'history',
+});
+
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router: xjRouter
 }).$mount('#app')
---
+++ src/routes.js
@@ -0,0 +1,12 @@
+import Dashboard from './components/Dashboard.vue';
+
+export default [
+  {
+    path: '/',
+    component: Dashboard
+  },
+  {
+    path: '/dashboard',
+    component: Dashboard
+  }
+]
