cd20db2ce43f101e0e05bc33ae8aa8596c7bfcc7
OSandh <oskarsandh@gmail.com>
2019-11-13 14:07:33 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <div id="app">
-
+    
   </div>
 </template>
 
 <script>
+// import Sidebar from './components/Sidebar.vue';
+// import Header from './components/Header.vue';
 
 export default {
   name: 'app',
+
   components: {
     
   }
